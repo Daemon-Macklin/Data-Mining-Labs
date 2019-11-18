347c865737b876cf155b0e3821c48ad3a4b0fe7d
Daemon-Macklin <daemon.macklin@outlook.com>
2019-11-18 18:12:27 +0000

    Adding 3

--- lab3/main.py
+++ lab3/main.py
@@ -7,6 +7,15 @@ from sklearn.metrics import classification_report
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 
+# irisSubsets is a list of the subsets
+# i = subset
+# 0 = X_train
+# 1 = y_train
+# 2 = X_test
+# 3 = y_test
+# 4 = X_valid
+# 5 = y_valid
+
 # Step 1 - Import the data from the dataset 
 def importIris():
     
@@ -22,7 +31,8 @@ def splitData(iris):
     y=iris.target
 
     # Split the data into two subsets test 50% and train 50% 
-    X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.5)
+    # Add a seed so that the data will be split the same way each time this is run
+    X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.5, random_state=7999)
     
     print(X_train)
     #print("")
@@ -30,7 +40,8 @@ def splitData(iris):
     #print("")
     
     # Now split the test data into two subsets test 25% and validataion 25%
-    X_test,X_valid,y_test,y_valid=train_test_split(X_test,y_test, test_size=0.5)
+    # Add a seed so that the data will be split the same way each time this is run
+    X_test,X_valid,y_test,y_valid=train_test_split(X_test,y_test, test_size=0.5, random_state=6287)
     
     # Now we have 3 subsets of the data
     # Train 50% of data
@@ -44,25 +55,11 @@ def splitData(iris):
     #print(X_valid)
     #print("")
     #print(y_valid)
+    # Return a list of the subsets
     return X_train, y_train, X_test, y_test, X_valid, y_valid
 
 # Step 3 - Ensure subsets are independent
 def subsetTest(irisSubsets, iris):
-    
-    print(iris.target)
-    print("")
-    # y_train
-    print(irisSubsets[1])
-    print("")
-    # y_test
-    print(irisSubsets[3])
-    print("")
-    # y_valid
-    print(irisSubsets[5])
-    print("")
-    
-    # From visual inspection we see that the subsets are arranged in random order, as the original dataset was
-    # Ordered from 0-2 this suggests that the data has been selected randomly
     
     values, counts = np.unique(iris.target, return_counts=True)
     print("Main Data")
@@ -90,8 +87,30 @@ def subsetTest(irisSubsets, iris):
     # When we count the number of each class in the subsets we see two thing. One the data amount of each 
     # class in a subset always changes on re running the script, meaning that the data is selected randomly
     # and Two the number of each class in each subset will always add up to 50 which was the number of each class
-    # in the data set. This implies that there is no overlap in the data. This also shows that the variability
-    # of the set if covered and thus is representative 
+    # in the data set. This implies that there is no overlap in the data. 
+    
+    # Also from using the train test split method we can say that the data is independent automatically.
+    """
+    Main Data
+    [0 1 2]
+    [50 50 50]
+
+    y_train
+    [0 1 2]
+    [23 27 25]
+
+    y_test
+    [0 1 2]
+    [14  9 14]
+
+    y_valid
+    [0 1 2]
+    [13 14 11]
+    """
+    
+    # Based on the ratos between the amounts of data in the subsets we can say that the data in the subsets
+    # Is representative of the actual data
+    
 
 
 # Step 4 - building a classifer with SVM
@@ -112,7 +131,7 @@ def buildFirstClssifier(irisSubsets):
     #print("")
     print("Test")
     
-    # Test classification with test subset
+    # Test classification with test subset. Used to check which is best
     prediction_test = clf.predict(irisSubsets[2])
     print(classification_report(irisSubsets[3], prediction_test))
     print("Accuracy Score:")
@@ -120,7 +139,7 @@ def buildFirstClssifier(irisSubsets):
     print("")
     print("Validataion")
     
-    # Test again with the validation subset
+    # Validate  with the validation subset. Used if this is best classifier to evalaute future performance 
     prediction_valid = clf.predict(irisSubsets[4])
     print(classification_report(irisSubsets[5], prediction_valid))
     print("Accuracy Score:")
@@ -140,7 +159,7 @@ def buildSecondClassifier(irisSubsets):
     # Print results
     print("Test")
     
-    # Test classification with test subset
+    # Test classification with test subset. Used to check which is best
     prediction_test = clf.predict(irisSubsets[2])
     print(classification_report(irisSubsets[3], prediction_test))
     print("Accuracy Score:")
@@ -148,7 +167,7 @@ def buildSecondClassifier(irisSubsets):
     print("")
     print("Validataion")
     
-    # Test again with the validation subset
+    # Validate with the validation subset. Used if this is best classifier to evalaute future performance
     prediction_valid = clf.predict(irisSubsets[4])
     print(classification_report(irisSubsets[5], prediction_valid))
     print("Accuracy Score:")
@@ -168,7 +187,7 @@ def buildThirdClassifier(irisSubsets):
     # Print results
     print("Test")
     
-    # Test classification with test subset
+    # Test classification with test subset. Used to check which is best
     prediction_test = clf.predict(irisSubsets[2])
     print(classification_report(irisSubsets[3], prediction_test))
     print("Accuracy Score:")
@@ -176,7 +195,7 @@ def buildThirdClassifier(irisSubsets):
     print("")
     print("Validataion")
     
-    # Test again with the validation subset
+    # Validate again with the validation subset. Used if this is best classifier to evalaute future performance
     prediction_valid = clf.predict(irisSubsets[4])
     print(classification_report(irisSubsets[5], prediction_valid))
     print("Accuracy Score:")
@@ -185,13 +204,50 @@ def buildThirdClassifier(irisSubsets):
 
 
 # Step 7 - Selecting the best classifier
-# Based on the Accuracy socres and classification reports I believe that logistic regression is the best
-# classifier. It achieved the highest accuracy score most of the time compared to the other two classifiers through
-# a number of tests. However with the amount of data used it is not entirly clear which is the best as sometimes the S
-# VM preforms best or the decision tree performs best.
+"""
+----------Logistic Regression----------
+
+Test
+              precision    recall  f1-score   support
+
+           0       1.00      1.00      1.00        14
+           1       1.00      1.00      1.00         9
+           2       1.00      1.00      1.00        14
+
+    accuracy                           1.00        37
+   macro avg       1.00      1.00      1.00        37
+weighted avg       1.00      1.00      1.00        37
+
+Accuracy Score:
+1.0
+
+Based on the Accuracy socres and classification reports I believe that logistic regression is the best
+classifier.
+"""
 
 # Step 8 - Future Performance on Logistic regression classifier
-# I can't see the future
+"""
+----------Logistic Regression----------
+
+Validataion
+              precision    recall  f1-score   support
+
+           0       1.00      1.00      1.00        13
+           1       0.93      1.00      0.97        14
+           2       1.00      0.91      0.95        11
+
+    accuracy                           0.97        38
+   macro avg       0.98      0.97      0.97        38
+weighted avg       0.98      0.97      0.97        38
+
+Accuracy Score:
+0.9736842105263158
+
+After testing with the validation data set the logistic regression classifier has a .97 accuracy score. This 
+suggests that it will perform well in future. It is less than the test accuarcy but it is still strong and
+thus suggests that the classification will remain accurate on other data sets, as well as proving the test
+accuaracy of 1.0 is valid since the classifier remains accurate with new data but still having some errors. 
+"""
 
 def main():
 
